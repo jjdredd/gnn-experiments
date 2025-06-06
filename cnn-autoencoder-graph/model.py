a9288e54606d4c3cdd3d90b101a12a8f166e19a9
jjdredd <the.guard@mail.ru>
2025-06-06 00:08:59 -0400

    Working on the model

--- cnn-autoencoder-graph/model.py
+++ cnn-autoencoder-graph/model.py
@@ -11,41 +11,28 @@ class CnnGraphEncoder(nn.Module):
         super().__init__()
         # input 32x32, output 14x14 ((32 - 8)/2)
         self.conv_layer_1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=48,
+            nn.Conv2d(in_channels=1, out_channels=16,
                       kernel_size=(5, 5),
                       stride=1, padding=0, bias=True),
             nn.ELU(),
-            nn.Conv2d(in_channels=48, out_channels=144,
+            nn.Conv2d(in_channels=16, out_channels=32,
                       kernel_size=(5, 5),
                       stride=1, padding=0, bias=True),
             nn.ELU(),
-            nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
-        )
-
-        # input 12x12, output ((12 - 6)# / 2)
-        self.conv_layer_2 = nn.Sequential(
-            nn.Conv2d(in_channels=144, out_channels=288,
+            nn.Conv2d(in_channels=32, out_channels=32,
                       kernel_size=(3, 3),
                       stride=1, padding=0, bias=True),
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
+            nn.ELU(),
+            nn.Conv2d(in_channels=32, out_channels=8,
+                      kernel_size=(3, 3),
+                      stride=1, padding=0, bias=True),
+            nn.ELU(),
         )
-
-        # input 3x3x240 (out_channels), flattening
-        self.fully_connected_stack = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=7200, out_features=3000),
-            nn.ReLU(),
-            nn.Linear(in_features=3000, out_features=1000),
-            nn.ReLU(),
-            nn.Linear(in_features=1000, out_features=classes),
-	    nn.LogSoftmax(dim=1))
 
     def forward(self, x):
         x = self.conv_layer_1(x)
-        x = self.conv_layer_2(x)
-        logits = self.fully_connected_stack(x)
-        return logits
+        
+        return x
 
 model = NeuralNetwork().to(device)
 print(model)
