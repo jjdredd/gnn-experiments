9288a9e80a67fe34375490c6cf11861822f77812
jjdredd <the.guard@mail.ru>
2025-06-07 22:10:27 -0400

    Model code

--- cnn-autoencoder-graph/model.py
+++ cnn-autoencoder-graph/model.py
@@ -5,10 +5,18 @@ from time import perf_counter_ns
 
 import data_loader as dl
 
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    device_name = torch.cuda.get_device_name(0)
+else:
+    device = torch.device("cpu")
+
+print(f"Using {device} device")
 
 class CnnGraphEncoder(nn.Module):
     def __init__(self):
         super().__init__()
+        self.sigmoid = nn.Sigmoid()
         # input 32x32, output 14x14 ((32 - 8)/2)
         self.conv_layer_1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=16,
@@ -31,8 +39,8 @@ class CnnGraphEncoder(nn.Module):
 
     def forward(self, x):
         x = self.conv_layer_1(x)
-        
-        return x
+        x = torch.flatten(x, end_dim=1)
+        return self.sigmoid(torch.einsum('i,j->ij', x, x))
 
 model = NeuralNetwork().to(device)
 print(model)
