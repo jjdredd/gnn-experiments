0c16374f35825e92ea62e128bf615a13d1d0399e
jjdredd <the.guard@mail.ru>
2025-07-09 00:40:35 -0400

    Generate matrices for gt edges and implement tests

--- gnn-mk1/graph_loss.py
+++ gnn-mk1/graph_loss.py
@@ -5,23 +5,31 @@ from torch import nn
 
 # no vector may have a zero coordinate!
 def EdgeMatrix2D(v_1, v_2):
-    return = torch.reciprocal(torch.matmul(torch.mul(v_1, v_2), torch.diag(torch.tensor([1, -1]))))
+    return torch.reciprocal(torch.matmul(torch.mul(v_1, v_2), torch.diag(torch.tensor([1.0, -1.0]))))
     
 
 def EdgeMatrices(vertices, edges):
     """
     Compute a loss function matrix for each edge.
+    vertices: vertex features (tensor, shape (n, m) )
+    edges: vertex indices forming edges (tensor, shape (2, n))
+    must return a tensor of matrices (shape (n, m, m))
     """
-    edge_matrices = []
-    for n in range(edges.shape[1]):
-        edge_matrices.append(EdgeMatrix2D(edges[0][n]))
+    V_1 = vertices[edges[0]]
+    V_2 = vertices[edges[1]]
+    # XXX FIXME
+    # filter out the repeating edges!!
+    matrix_list = []
+    for v_1, v_2 in zip(V_1, V_2):
+        matrix_list.append(EdgeMatrix2D(v_1, v_2))
+    return torch.stack(matrix_list, dim=0)
 
 
 class GraphLoss(nn.Module):
     def __init__(self):
         super(CustomLoss, self).__init__()
 
-    def forward(self, vertices, edges, edge_features, targets):
+    def forward(self, vertices, edges, edge_features, edge_matrices):
         """
         Function to calculate the loss.
         Arguments:
@@ -30,6 +38,8 @@ class GraphLoss(nn.Module):
         edge_features: edge probability (tensor, shape (n))
         targets: matrices representing the ground truth edges
         """
-        pass
+        V_1 = vertices[edges[0]]
+        V_2 = vertices[edges[1]]
+
 
 
--- gnn-mk1/tests/graph_loss_test.py
+++ gnn-mk1/tests/graph_loss_test.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python3
 
 import unittest
+import torch
+import graph_loss as gl
 
-class 
+class TestEdgeMatrices(unittest.TestCase):
+    def test_SingleEdge(self):
+        vertices = torch.stack([torch.randn(2) for _ in range(2)], dim=0)
+        edges = torch.tensor([[0, 1], [1, 0]])
+        matrices = gl.EdgeMatrices(vertices, edges)
+        loss = torch.matmul(vertices[0], torch.mul(matrices[0], vertices[1]))
+        print(loss, matrices)
+        self.assertTrue(abs(loss) < 10**(-5))
+
+    def test_MultipleEdge(self):
+        # todo
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
