c9c2ec8f7b85f9f213e9fc8295f21b8fb52c3ead
jjdredd <the.guard@mail.ru>
2025-07-09 23:41:35 -0400

    Filter repeated edges for graph loss and test

--- gnn-mk1/graph_loss.py
+++ gnn-mk1/graph_loss.py
@@ -6,7 +6,24 @@ from torch import nn
 # no vector may have a zero coordinate!
 def EdgeMatrix2D(v_1, v_2):
     return torch.reciprocal(torch.matmul(torch.mul(v_1, v_2), torch.diag(torch.tensor([1.0, -1.0]))))
-    
+
+def FilterRepeatedEdges(edges):
+    """
+    Returns edges filtered, without duplicates
+    """
+    result = edges[:, 0].unsqueeze(dim=1)
+    edge_set = set()
+    edge_set.add((result[0, 0].item(), result[1, 0].item()))
+    for n in range(1, edges.shape[1]):
+        reverse_edge = (edges[1, n].item(), edges[0, n].item())
+        normal_edge = (edges[0, n].item(), edges[1, n].item())
+        if reverse_edge in edge_set or normal_edge in edge_set:
+            continue
+        else:
+            edge_set.add(normal_edge)
+            result = torch.cat((result, edges[:, n].unsqueeze(dim=1)), dim=1)
+    return result
+        
 
 def EdgeMatrices(vertices, edges):
     """
@@ -15,10 +32,9 @@ def EdgeMatrices(vertices, edges):
     edges: vertex indices forming edges (tensor, shape (2, n))
     must return a tensor of matrices (shape (n, m, m))
     """
-    V_1 = vertices[edges[0]]
-    V_2 = vertices[edges[1]]
-    # XXX FIXME
-    # filter out the repeating edges!!
+    filtered_edges = FilterRepeatedEdges(edges)
+    V_1 = vertices[filtered_edges[0]]
+    V_2 = vertices[filtered_edges[1]]
     matrix_list = []
     for v_1, v_2 in zip(V_1, V_2):
         matrix_list.append(EdgeMatrix2D(v_1, v_2))
@@ -26,8 +42,9 @@ def EdgeMatrices(vertices, edges):
 
 
 class GraphLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, singularity_cutoff=1000):
         super(CustomLoss, self).__init__()
+        self.singularity_cutoff = singularity_cutoff
 
     def forward(self, vertices, edges, edge_features, edge_matrices):
         """
@@ -38,8 +55,9 @@ class GraphLoss(nn.Module):
         edge_features: edge probability (tensor, shape (n))
         targets: matrices representing the ground truth edges
         """
-        V_1 = vertices[edges[0]]
-        V_2 = vertices[edges[1]]
+        filtered_edges = FilterRepeatedEdges(edges)
+        V_1 = vertices[filtered_edges[0]]
+        V_2 = vertices[filtered_edges[1]]
 
 
 
--- gnn-mk1/tests/graph_loss_test.py
+++ gnn-mk1/tests/graph_loss_test.py
@@ -10,13 +10,28 @@ class TestEdgeMatrices(unittest.TestCase):
         edges = torch.tensor([[0, 1], [1, 0]])
         matrices = gl.EdgeMatrices(vertices, edges)
         loss = torch.matmul(vertices[0], torch.mul(matrices[0], vertices[1]))
-        print(loss, matrices)
+        # print(loss, matrices)
         self.assertTrue(abs(loss) < 10**(-5))
 
-    def test_MultipleEdge(self):
-        # todo
-        pass
+    def test_FilterRepeatedEdges(self):
+        edges = torch.tensor([[0, 1, 1, 2, 4, 5, 3, 8, 10],
+                              [1, 2, 3, 1, 10, 7, 1, 6, 4]])
+        edges_expected = torch.tensor([[0, 1, 1, 4, 5, 8],
+                                       [1, 2, 3, 10, 7, 6]])
+        edges_processed = gl.FilterRepeatedEdges(edges)
+        self.assertTrue(torch.all(torch.isclose(edges_processed, edges_expected)).item())
 
+    def test_GraphLoss_MinVal(self):
+        loss = gl.GraphLoss(singularity_cutoff=100)
+        vertices = torch.stack([torch.randn(2) for _ in range(2)], dim=0)
+        edges = torch.tensor([[0, 1], [1, 0]])
+        edge_features = torch.tensor([1.0, 1.0]) # or edge weights in this case
+        expected_min_val = 
+        edge_matrices = EdgeMatrices(vertices, edges)
+        gt_loss_val = loss.forward(vertices, edges, edge_features, edge_matrices)
+
+    def test_GraphLoss
+        # manipulate the vertices/edges and make sure the new loss is greater than the gt_loss_val
 
 if __name__ == "__main__":
     unittest.main()
