9784fccb98de3bad63837f05234676f48257d33f
jjdredd <the.guard@mail.ru>
2025-08-06 23:11:49 -0400

    yolo data generator

--- edge-object-detection/gt-generator.py
+++ edge-object-detection/gt-generator.py
@@ -27,8 +27,13 @@ class YoloDataGenerator():
     def PixelCompare(x, y):
         return abs(x - y) < 1.0
 
+    @staticmethod
     def CalculateCenter(p_1, p_2):
-        return ( (p_1[0] + p_2[0]) / 2.0, (p_1[1] + p_2[1]) / 2.0 )
+        return ((p_1[0] + p_2[0]) / 2.0, (p_1[1] + p_2[1]) / 2.0)
+
+    @staticmethod
+    def EdgeBoungdingBox(p_1, p_2):
+        return (p_2[0] - p_1[0], p_2[1] - p_1[1])
 
     def PointInLimits(self, x, y) -> bool:
         return (x > 1 and x < self.image_size - 2 and y > 1 and y < self.image_size - 2)
@@ -45,10 +50,13 @@ class YoloDataGenerator():
             return 2
         elif YoloDataGenerator.PixelCompare(p_1[0], p_2[0]):
             return 3
-        elif (p_1[0] - p_2[0]) * (p_1[1] - p_2[1]) > 0:
+        elif (p_2[0] - p_1[0]) * (p_2[1] - p_1[1]) > 0:
             return 0
         else:
             return 1
+
+    def NormalizeCoordinates(self, p):
+        return (p[0] / self.image_size, p[1] / self.image_size)
 
     def GenerateSample(self, base_name, directory_path, training):
         subdirectory = 'train' if training else 'val'
@@ -65,57 +73,24 @@ class YoloDataGenerator():
             y_2 = random.randint(5, self.image_size - 5)
 
         edge_class = self.ClassifyEdge((x_1, y_1), (x_2, y_2))
-        edge_center = YoloDataGenerator.CalculateCenter((x_1, y_1), (x_2, y_2))
-        txt_file.write(f'{edge_class}\t{edge_center[0]}\t{edge_center[1]}\t{x_2}\t{y_2}\n')
+        edge_center = self.NormalizeCoordinates(YoloDataGenerator.CalculateCenter((x_1, y_1), (x_2, y_2)))
+        edge_bb = self.NormalizeCoordinates(YoloDataGenerator.EdgeBoungdingBox((x_1, y_1), (x_2, y_2)))
+        # need to normalize coordinates
+        txt_file.write(f'{edge_class}\t{edge_center[0]}\t{edge_center[1]}\t{edge_bb[0]}\t{edge_bb[1]}\n')
 
         image = np.zeros((ImageSize, ImageSize, 3), dtype=np.uint8)  # Example: black image
-
         # Define start and end points of the line
         start_point = (x_1, y_1)
         end_point = (x_2, y_2)
-
         # Define line color (BGR format)
         color = (255, 255, 255)
-
         # Define line thickness
         thickness = 1
-
         # Draw the line
         cv2.line(image, start_point, end_point, color, thickness)
-        cv2.imwrite(f'{OutputDirectory}/line_{i}.png', image)
+        cv2.imwrite(f'{image_file_directory}/{base_name}.png', image)
 
-
-
-OutputDirectory = sys.argv[1]
-
-
-
-for i in range(NumSamples):
-    x_1 = x_2 = y_1 = y_2 = 0
-    while (abs(x_1 - x_2) < MinLineDim
-           and abs(y_1 - y_2) < MinLineDim
-           and not (PointInLimits(x_1, y_1) or PointInLimits(x_2, y_2))):
-        x_1 = random.randint(0, ImageSize - 1)
-        x_2 = random.randint(0, ImageSize - 1)
-        y_1 = random.randint(0, ImageSize - 1)
-        y_2 = random.randint(0, ImageSize - 1)
-
-    txt_file.write(f'{i}\t{x_1}\t{y_1}\t{x_2}\t{y_2}\n')
-
-    image = np.zeros((ImageSize, ImageSize, 3), dtype=np.uint8)  # Example: black image
-
-    # Define start and end points of the line
-    start_point = (x_1, y_1)
-    end_point = (x_2, y_2)
-
-    # Define line color (BGR format)
-    color = (255, 255, 255)
-
-    # Define line thickness
-    thickness = 1
-
-    # Draw the line
-    cv2.line(image, start_point, end_point, color, thickness)
-    cv2.imwrite(f'{OutputDirectory}/line_{i}.png', image)
+    def GenerateDataSets(self, directory_path):
+        pass
 
 
